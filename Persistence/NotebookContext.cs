0858a9b295864b1c11e7a6774867bf97199785bd
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-09-30 20:01:22 -0300

    add: context

--- Persistence/NotebookContext.cs
+++ Persistence/NotebookContext.cs
@@ -2,11 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Api_test.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api_test.Persistence
 {
-    public class NotebookContext
+    public class NotebookContext : DbContext
     {
-        
-    }
+        public NotebookContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Notebook> Notebooks {get;set;}
+}
 }
